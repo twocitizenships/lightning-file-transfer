e22e39b84c4d9c7d9333adfcc5421e4e272b014c
clusk <clusk@strike.me>
2021-10-08 22:29:55 -0400

    Create Data project

---
+++ lightning-transfer/LightningTransfer.Data/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LightningTransfer.Data
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
